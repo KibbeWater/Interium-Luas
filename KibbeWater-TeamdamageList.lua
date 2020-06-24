187744803aadafcd2371609f3072eb8d545cc794
Snow <TheCyberDiamondOfficial@outlook.com>
2020-06-24 16:44:14 +0200

    Added new design "Simple"

--- KibbeWater-TeamdamageList.lua
+++ KibbeWater-TeamdamageList.lua
@@ -3,8 +3,13 @@ Menu.Spacing()
 Menu.Separator()
 Menu.Spacing()
 Menu.Checkbox("Enable Teamdamage List", "cEnableTDamageList", true)
-Menu.Text("Color Scheme")
-Menu.Combo( "", "cTDamageDesign", { "Default", "Light", "RGB" }, 0)
+Menu.Checkbox("Lock Position", "cTDamageLock", false)
+Menu.Spacing()
+Menu.Combo( "Colors", "cTDamageColorScheme", { "Default", "Light", "RGB" }, 0)
+Menu.Spacing()
+Menu.Combo( "Design", "cTDamageDesign", { "Default", "Simple" }, 0)
+Menu.Spacing()
+Menu.Combo( "Format", "cTDamageFormat", { "{name} K:{kills} D:{deaths}", "{name}: {damage} ({kills})" }, 0)
 Menu.Separator()
 Menu.Text("INCASE TEAM DAMAGE LIST DOESN'T APPEAR")
 Menu.Checkbox("Reset Position", "cTDamageListReset", false)
@@ -118,21 +123,6 @@ Hack.RegisterCallback("PaintTraverse", function ()
 
     --Dragging System
     local cursor = InputSys.GetCursorPos()
-    
-    --Check if box is able to be dragged
-    if cursor.x >= posX and cursor.x <= posX + sizeX then
-        if cursor.y >= posY and cursor.y <= posY + sizeY then
-            if InputSys.IsKeyDown(1) and not Menu.GetBool("cSpeclistLockPublic") then
-                Dragging = "t" --supposed to be a bool but I was way to fucking lazy to change it from my old string system (I did parsing be fucking proud atleast)
-            else
-                Dragging = "f"
-            end
-        else
-            if InputSys.IsKeyDown(0) or OldDragging == "f" then Dragging = "f" end
-        end
-    else
-        if InputSys.IsKeyDown(0) or OldDragging == "f" then Dragging = "f" end
-    end
 
     local data = {}
     local banned = {}
@@ -147,8 +137,13 @@ Hack.RegisterCallback("PaintTraverse", function ()
         local username = GetUsername(i)
         
         if kills[i] > 0 or damage[i] > 0 then
-            if kills[i] > 2 or damage[i] > 299 then table.insert(banned, true) else table.insert(banned, false) end
-            if kills[i] > 2 or damage[i] > 299 then table.insert(data, ShortenString(username, 18) .. " K:" .. kills[i] .. " D:" .. damage[i] .. " BANNED") else table.insert(data, ShortenString(username, 18) .. " K:" .. kills[i] .. " D:" .. damage[i]) end
+            if Menu.GetInt("cTDamageFormat") == 0 then
+                if kills[i] > 2 or damage[i] > 299 then table.insert(banned, true) else table.insert(banned, false) end
+                if kills[i] > 2 or damage[i] > 299 then table.insert(data, ShortenString(username, 18) .. " K:" .. kills[i] .. " D:" .. damage[i] .. " BANNED") else table.insert(data, ShortenString(username, 18) .. " K:" .. kills[i] .. " D:" .. damage[i]) end
+            elseif Menu.GetInt("cTDamageFormat") == 1 then
+                if kills[i] > 2 or damage[i] > 299 then table.insert(banned, true) else table.insert(banned, false) end
+                if kills[i] > 2 or damage[i] > 299 then table.insert(data, ShortenString(username, 18) .. ": " .. kills[i] .. " (" .. damage[i] .. ") BANNED") else table.insert(data, ShortenString(username, 18) .. ": " .. kills[i] .. " (" .. damage[i] .. ")") end
+            end
         end
 
         ::skip::
@@ -156,57 +151,128 @@ Hack.RegisterCallback("PaintTraverse", function ()
     
     --RGB Drawing (why does everyone like rbg xP)
     local S = 3
-
+    
     S,Type[1], R[1], G[1], B[1] = Rainbow(3,Type[1], R[1], G[1], B[1])
     S,Type[4], R[4], G[4], B[4] = Rainbow(3,Type[4], R[4], G[4], B[4])
-
-    --Draw Extension
-    local extensionSizeY = 0
-    local highestX = 0
     
-    for i = 1, #data do
-        extensionSizeY = extensionSizeY + 6
-        local textSizeS = Render.CalcTextSize(data[i], 16, "TDamageFont")
-        if highestX < textSizeS.x then highestX = textSizeS.x end
-        extensionSizeY = extensionSizeY + textSizeS.y
-    end
-    extensionSizeY = extensionSizeY + 6
-
-    local xSize = sizeX
-    if highestX + 12 > sizeX then xSize = highestX + 12 end
-    
-    local clr = Color.new(255,255,255,255)
-    local clrBG = Color.new(0,0,0,255)
-    if Menu.GetInt("cTDamageDesign") == 0 then 
-        clr = Color.new(255,255,255,255) 
-        clrBG = Color.new(0,0,0,255)
-    elseif Menu.GetInt("cTDamageDesign") == 1 then 
-        clr = Color.new(0,0,0,255) 
-        clrBG = Color.new(255,255,255,255)
-    end
-
-    Render.RectFilled(posX, posY, posX + xSize, posY + sizeY, clrBG, 0)
-    if Menu.GetInt("cTDamageDesign") == 2 then Render.RectFilledMultiColor(posX + 4, posY + 7, (posX + xSize) - 4, posY + sizeY - 7, Color.new(R[4], G[4], B[4], 255),Color.new(R[1], G[1], B[1], 255),Color.new(R[1], G[1], B[1], 255),Color.new(R[4], G[4], B[4], 255)) else Render.RectFilled(posX + 4, posY + 7, (posX + xSize) - 4, posY + sizeY - 7, clr, 0) end
-
-    Render.RectFilled(posX, posY + sizeY, posX + xSize, posY + sizeY + extensionSizeY, clrBG, 0)
-
-    --Draw Data
-    extensionSizeY = 0
-
-    for i = 1, #data do
-        extensionSizeY = extensionSizeY + 6
-        local textSizeS = Render.CalcTextSize(data[i], 16, "TDamageFont")
-        local textClr = clr
+    if Menu.GetInt("cTDamageDesign") == 0 then --Default
+        sizeY = 16
         
-        if banned[i] then textClr = Color.new(255, 0, 0, 255) end
-        Render.Text(data[i], posX + 6, posY + extensionSizeY + sizeY, 16, textClr, false, false, "TDamageFont")
-        extensionSizeY = extensionSizeY + textSizeS.y
+        --Draw Extension
+        local extensionSizeY = 0
+        local highestX = 0
+        
+        for i = 1, #data do
+            extensionSizeY = extensionSizeY + 6
+            local textSizeS = Render.CalcTextSize(data[i], 16, "TDamageFont")
+            if highestX < textSizeS.x then highestX = textSizeS.x end
+            extensionSizeY = extensionSizeY + textSizeS.y
+        end
+        extensionSizeY = extensionSizeY + 6
+        
+        local xSize = sizeX
+        if highestX + 12 > sizeX then xSize = highestX + 12 end
+    
+        local clr = Color.new(255,255,255,255)
+        local clrBG = Color.new(0,0,0,255)
+        if Menu.GetInt("cTDamageColorScheme") == 0 then 
+            clr = Color.new(255,255,255,255) 
+            clrBG = Color.new(0,0,0,255)
+        elseif Menu.GetInt("cTDamageColorScheme") == 1 then 
+            clr = Color.new(0,0,0,255) 
+            clrBG = Color.new(255,255,255,255)
+        end
+
+        Render.RectFilled(posX, posY, posX + xSize, posY + sizeY, clrBG, 0)
+        if Menu.GetInt("cTDamageColorScheme") == 2 then Render.RectFilledMultiColor(posX + 4, posY + 7, (posX + xSize) - 4, posY + sizeY - 7, Color.new(R[4], G[4], B[4], 255),Color.new(R[1], G[1], B[1], 255),Color.new(R[1], G[1], B[1], 255),Color.new(R[4], G[4], B[4], 255)) else Render.RectFilled(posX + 4, posY + 7, (posX + xSize) - 4, posY + sizeY - 7, clr, 0) end
+
+        Render.RectFilled(posX, posY + sizeY, posX + xSize, posY + sizeY + extensionSizeY, clrBG, 0)
+
+        --Draw Data
+        extensionSizeY = 0
+
+        for i = 1, #data do
+            extensionSizeY = extensionSizeY + 6
+            local textSizeS = Render.CalcTextSize(data[i], 16, "TDamageFont")
+            local textClr = clr
+        
+            if banned[i] then textClr = Color.new(255, 0, 0, 255) end
+            Render.Text(data[i], posX + 6, posY + extensionSizeY + sizeY, 16, textClr, false, false, "TDamageFont")
+            extensionSizeY = extensionSizeY + textSizeS.y
+        end
+        extensionSizeY = extensionSizeY + 6
+
+        --Check if box is able to be dragged
+        if cursor.x >= posX and cursor.x <= posX + sizeX then
+            if cursor.y >= posY and cursor.y <= posY + sizeY then
+                if InputSys.IsKeyDown(1) and not Menu.GetBool("cTDamageLock") then
+                    Dragging = "t" --supposed to be a bool but I was way to fucking lazy to change it from my old string system (I did parsing be fucking proud atleast)
+                else
+                    Dragging = "f"
+                end
+            else
+                if InputSys.IsKeyDown(0) or OldDragging == "f" then Dragging = "f" end
+            end
+        else
+            if InputSys.IsKeyDown(0) or OldDragging == "f" then Dragging = "f" end
+        end
+    elseif Menu.GetInt("cTDamageDesign") == 1 then --Simple
+        --Draw Extension
+        local extensionSizeY = 3 + Render.CalcTextSize("Team Damage List:", 16, "TDamageFont").y
+        local highestX = sizeX
+        local boxSize = 2.5
+        
+        for i = 1, #data do
+            extensionSizeY = extensionSizeY + 1
+            local textSizeS = Render.CalcTextSize(data[i], 16, "TDamageFont")
+            if highestX < textSizeS.x then highestX = textSizeS.x end
+            extensionSizeY = extensionSizeY + textSizeS.y
+        end
+        extensionSizeY = extensionSizeY + 1
+        sizeY = extensionSizeY
+        
+        Render.Text("Team Damage List:", posX, posY, 16, Color.new(255,255,255,255), false, false, "TDamageFont")
+
+        if Globals.MenuOpened() then 
+            Render.Rect(posX, posY, posX + highestX, posY + extensionSizeY, Color.new(255,255,255,255), 0, 2)
+        
+            Render.RectFilled(posX - boxSize, posY - boxSize, posX + boxSize, posY + boxSize, Color.new(255,255,255,255), 0)
+            Render.RectFilled(posX - boxSize + highestX, posY - boxSize, posX + boxSize + highestX, posY + boxSize, Color.new(255,255,255,255), 0)
+
+            Render.RectFilled(posX - boxSize, posY - boxSize + extensionSizeY, posX + boxSize, posY + boxSize + extensionSizeY, Color.new(255,255,255,255), 0)
+            Render.RectFilled(posX - boxSize + highestX, posY - boxSize + extensionSizeY, posX + boxSize + highestX, posY + boxSize + extensionSizeY, Color.new(255,255,255,255), 0)
+        end
+
+        extensionSizeY = 3 + Render.CalcTextSize_1("Team Damage List:", 16).y
+        for i = 1, #data do
+            extensionSizeY = extensionSizeY + 1
+            local textSizeS = Render.CalcTextSize(data[i], 16, "TDamageFont")
+            if highestX < textSizeS.x then highestX = textSizeS.x end
+            Render.Text(data[i], posX, posY + extensionSizeY, 16, Color.new(255,255,255,255), false, false, "TDamageFont")
+            extensionSizeY = extensionSizeY + textSizeS.y
+        end
+        extensionSizeY = extensionSizeY + 1
+
+        --Check if box is able to be dragged
+        if cursor.x >= posX and cursor.x <= posX + highestX then
+            if cursor.y >= posY and cursor.y <= posY + extensionSizeY then
+                if InputSys.IsKeyDown(1) and not Menu.GetBool("cTDamageLock") and Globals.MenuOpened() then
+                    Dragging = "t" --supposed to be a bool but I was way to fucking lazy to change it from my old string system (I did parsing be fucking proud atleast)
+                else
+                    Dragging = "f"
+                end
+            else
+                if InputSys.IsKeyDown(0) or OldDragging == "f" then Dragging = "f" end
+            end
+        else
+            if InputSys.IsKeyDown(0) or OldDragging == "f" then Dragging = "f" end
+        end
+
     end
-    extensionSizeY = extensionSizeY + 6
 
     --Coderman optimize b4 release
     if nextAutosave <= IGlobalVars.realtime then
-        FileSys.SaveTextToFile(GetAppData() .. "\\INTERIUM\\CSGO\\FilesForLUA\\kibbewater\\data.s", posX .. "," .. posY)
+        FileSys.SaveTextToFile(GetAppData() .. "\\INTERIUM\\CSGO\\FilesForLUA\\kibbewater\\dataTDamage.s", posX .. "," .. posY)
         nextAutosave = IGlobalVars.realtime + secBeforeAutoSave
     end
 end)
