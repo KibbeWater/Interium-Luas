e526efd274dfa8d5d95daa2b2b47d178471313bf
Snow <TheCyberDiamondOfficial@outlook.com>
2022-05-26 18:56:01 +0200

    Added Cubic Bezier Curves

--- KibbeWater-AnimationLib.lua
+++ KibbeWater-AnimationLib.lua
@@ -1,4 +1,4 @@
-local Animator = {starttime = 0, durbation=1, FPS=300, reverse=false, data={frames=0,frame=0}}
+local Animator = { starttime = 0, durbation = 1, FPS = 300, reverse = false, keepBezier = true, p1 = { x = 0.5, y = 0.5 }, p2 = { x = 0.5, y = 0.5 }, data = { frames = 0, frame = 0 } }
 
 function Deepcopy(orig)
     local orig_type = type(orig)
@@ -15,12 +15,51 @@ function Deepcopy(orig)
     return copy
 end
 
-local function clamp(n,min,max)
+local function clamp(n, min, max)
     if n < min then return min elseif n > max then return max else return n end
+end
+
+local function interpolate(p1, p2, t)
+    return { x = p1.x + (p2.x - p1.x) * t, y = p1.y + (p2.y - p1.y) * t }
+end
+
+local function ComputeLines(lines, t)
+    if #lines > 2 then
+        local newLines = {}
+        for i = 2, #lines do
+            local p1 = lines[i - 1]
+            local p2 = lines[i]
+            table.insert(newLines, { p1 = interpolate(p1.p1, p1.p2, t), p2 = interpolate(p2.p1, p2.p2, t) })
+        end
+        return ComputeLines(newLines, t)
+    else
+        local point1Diff = interpolate(lines[1].p1, lines[1].p2, t)
+        local point2Diff = interpolate(lines[2].p1, lines[2].p2, t)
+        return interpolate(point1Diff, point2Diff, t)
+    end
+end
+
+local function BezierCurve(start, endd, p, t)
+    local points = { start }
+    for i = 1, #p do table.insert(points, p[i]) end
+    table.insert(points, endd)
+
+    local lines = {}
+    for i = 2, #points do
+        local curPoint = points[i]
+        local prevPoint = points[i - 1]
+        table.insert(lines, { p1 = prevPoint, p2 = curPoint })
+    end
+    return ComputeLines(lines, t)
 end
 
 function Animator:New()
     return Deepcopy(self)
+end
+
+function Animator:SetBezier(x, y, x2, y2)
+    self.p1 = { x = x, y = y }
+    self.p2 = { x = x2, y = y2 }
 end
 
 function Animator:Start(FPS, durbation)
@@ -49,31 +88,11 @@ end
 
 function Animator:GetValue(min, max)
     self:Update()
-    return (((max-min) / 100)*(100-((100 / self.data.frames) * self.data.frame)))+min
+
+    local t = self.data.frame / self.data.frames
+    if self.reverse and self.keepBezier then t = 1 - t end
+    local val = 1 - BezierCurve({ x = 0, y = 0 }, { x = 1, y = 1 }, { self.p1, self.p2 }, t).y
+    if self.reverse and self.keepBezier then val = 1 - val end
+
+    return (((max - min) / 100) * (100 - val * 100)) + min
 end
-
-local animFade = Animator:New()
-animFade:Start(300, 0.4)
-local animFade2 = Animator:New()
-animFade2:Start(300, 0.4)
-local animFade3 = Animator:New()
-animFade3:Start(300, 1)
-
-Hack.RegisterCallback("PaintTraverse", function ()
-    local opacity = animFade:GetValue(0,255)
-    local sizeX = animFade:GetValue(0,200)
-    local opacity2 = animFade2:GetValue(0,255)
-    local sizeX2 = animFade2:GetValue(0,400)
-    local opacity3 = animFade3:GetValue(0,255)
-    local sizeX3 = animFade3:GetValue(0,400)
-
-    Render.RectFilled(0, 400, sizeX, 440, Color.new(255,255,255,opacity))
-    Render.RectFilled(0, 480, sizeX2, 440, Color.new(255,255,255,opacity2))
-    Render.RectFilled(0, 480, sizeX3, 520, Color.new(255,255,255,opacity3))
-
-    if animFade:Finished() and animFade2:Finished() and animFade3:Finished() then
-        animFade:Reverse()
-        animFade2:Reverse()
-        animFade3:Reverse()
-    end
-end)
